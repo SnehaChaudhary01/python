388760c52adfe542dcd7368594f81f1054903958
SnehaChaudhary01 <cdysneha55@gmail.com>
2026-06-11 22:32:49 +0545

    Added Student class

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # python
 A collection of Python projects built using Object-Oriented Programming (OOP) concepts.
+## Technologies Used
+- Python 3
+- Object-Oriented Programming (OOP)
+- Git & GitHub
---
+++ students.py
@@ -0,0 +1,8 @@
+class Student:
+    all_students = []
+    def __init__(self, student_id, name, age, course, marks):
+        self.student_id = student_id
+        self.name = name
+        self.age = age
+        self.course = course
+        self.marks = marks
